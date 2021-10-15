1af4cf5284aa9487e72ccc227d434d2fa7db6d60
kai <flynn98721@gmail.com>
2021-10-14 21:44:25 -0400

    prod ready

--- CarLife Node/_helpers/jwt.js
+++ CarLife Node/_helpers/jwt.js
@@ -1,6 +1,6 @@
 const expressJwt = require('express-jwt');
 const userService = require('../services/user.service');
-const config = require('../config.json');
+const config = require('../../CarLife Angular/config.json');
 
 module.exports = jwt;
 
